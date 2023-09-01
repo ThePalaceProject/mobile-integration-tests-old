12120674daad7feb6f9ef821fa54404a069f407d
Astapkina Ekaterina <33911738+AEkaterina@users.noreply.github.com>
2023-09-01 13:06:48 +0400

    Update AndroidApplicationSteps.java

--- src/test/java/stepdefinitions/application/components/impl/AndroidApplicationSteps.java
+++ src/test/java/stepdefinitions/application/components/impl/AndroidApplicationSteps.java
@@ -49,6 +49,11 @@ public class AndroidApplicationSteps extends AbstractApplicationSteps {
     }
 
     @Override
+    public void closeAlert() {
+
+    }
+
+    @Override
     public boolean isMenuBarDisplayed() {
         return bottomMenuForm.isMenuDisplayed();
     }
