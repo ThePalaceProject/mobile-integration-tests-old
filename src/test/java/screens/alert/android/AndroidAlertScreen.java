831d2c7ece8a90bed7ad43e7270cc9e2d12fd905
Astapkina Ekaterina <33911738+AEkaterina@users.noreply.github.com>
2023-09-18 00:23:09 +0400

    Update AndroidAlertScreen.java

--- src/test/java/screens/alert/android/AndroidAlertScreen.java
+++ src/test/java/screens/alert/android/AndroidAlertScreen.java
@@ -1,6 +1,7 @@
 package screens.alert.android;
 
 import aquality.appium.mobile.application.PlatformName;
+import aquality.appium.mobile.elements.interfaces.IButton;
 import aquality.appium.mobile.screens.screenfactory.ScreenType;
 import enums.localization.catalog.EnumActionButtonsForBooksAndAlertsKeys;
 import org.openqa.selenium.By;
@@ -8,14 +9,19 @@ import screens.alert.AlertScreen;
 
 @ScreenType(platform = PlatformName.ANDROID)
 public class AndroidAlertScreen extends AlertScreen {
-    //todo I should check a unique element of screen for android
+    private static final String UNIQUE_ELEMENT_LOC = "//android.widget.LinearLayout[contains(@resource-id, \"grant_dialog\")]";
+    private static final String ACTION_BUTTON_LOC = UNIQUE_ELEMENT_LOC + "//android.widget.Button[@text=\"%s\"]";
+    
     public AndroidAlertScreen() {
-        super(By.id("android:id/message"));
+        super(By.xpath("//android.widget.LinearLayout[contains(@resource-id, \"grant_dialog\")]"));
     }
 
     @Override
     public void waitAndPerformAlertActionIfDisplayed(EnumActionButtonsForBooksAndAlertsKeys actionButtonNamesAlertKeys) {
-        //only ios
+        IButton actionButton = getElementFactory().getButton(By.xpath(String.format(ACTION_BUTTON_LOC, actionButtonNamesAlertKeys.getDefaultLocalizedValue())), String.format("%s ActionButtonAlert", actionButtonNamesAlertKeys.getDefaultLocalizedValue()));
+        if(actionButton.state().waitForDisplayed()){
+            actionButton.click();
+        }
     }
 
     @Override
